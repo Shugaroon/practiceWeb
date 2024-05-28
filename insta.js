8bc89bb915c52d2cbcfd56f4885db1657714a162
Shugaroon <rnballard@naver.com>
2024-05-28 13:53:43 +0900

    insta clone1

--- insta.js
+++ insta.js
@@ -1,4 +1,5 @@
 const leftContainerImg = document.querySelector(".left-container-img");
+
 console.log(leftContainerImg)
 
 let selected = 0;
@@ -6,7 +7,7 @@ const pics = ["./insta_pic.png", "./insta_pic2.gif"];
 
 
 setInterval(() => {
-    leftContainerImg.style.backgroundImage = `url(${pics[selected]})`;
+    img = `url(${pics[selected]})`;
 }, 10000);
 
 // 외않되.....
