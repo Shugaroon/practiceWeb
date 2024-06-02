d6020213c765eb2eb5c62d313ce86466d2baa171
Shugaroon <rnballard@naver.com>
2024-06-02 20:27:20 +0900

    upload2

--- pages/bootstrap.js
+++ pages/bootstrap.js
@@ -1,27 +1,91 @@
 const carousel = document.querySelector(".carousel");
 const arrows = document.querySelectorAll(".arrow");
+const topPicks = document.getElementById("topPicks");
+const newArrivals = document.getElementById("newArrivals");
+
+
 const pics = [
-    "http://127.0.0.1:5501/images/denimJackets.jpg", 
-    "http://127.0.0.1:5501/images/newjeans.jpg", 
-    "http://127.0.0.1:5001/images/converse.jpg"];
+    "/images/denim-jackets.jpg", 
+    "/images/palace-gap.jpg", 
+    "/images/converse.jpg"];
+const bottomPics = ["/images/top-picks.jpg", "/images/new-arrivals.jpg"];
+
+
 carousel.style.backgroundImage = `url(${pics[0]})`;
-carousel.style.backgroundsize = "cover";
+// carousel.style.backgroundsize = "cover";
+topPicks.style.backgroundImage = `url(${bottomPics[0]})`;
+newArrivals.style.backgroundImage = `url(${bottomPics[1]})`;
 
 
-let i = 0;
+let selected = 0;
+
 
 arrows.forEach(
-    arrow=>{
+    (arrow)=>{
         arrow.addEventListener("click", (event)=>{
-            if(event.target.id === "left"){
-                i--;
-                if(i<0){i = pics.length+1;}
+            if(event.target.id === "left-arrow"){
+                selected--;
+                if(selected < 0)
+                    {
+                    selected = pics.length-1;
+                }
             }
-            else if(event.target.id === "right"){
-                i++;
-                if(i >= pics.length){i = 0;}
+            else if(event.target.id === "right-arrow"){
+                selected++;
+                if(selected >= pics.length){
+                    selected = 0;
+                }
             }
-            container.style.backgroundImage = `url(${pics[i]})`;
+            carousel.style.backgroundImage = `url(${pics[selected]})`;
         })
     }
 )
+
+
+const modals = document.querySelectorAll(".modal");
+const nav1 = document.querySelector(".nav1");
+const nav2 = document.querySelector(".nav2");
+const nav3 = document.querySelector(".nav3");
+const nav4 = document.querySelector(".nav4");
+const nav5 = document.querySelector(".nav5");
+const nav6 = document.querySelector(".nav6");
+
+
+
+nav1.forEach(nav1)
+    nav1=>{
+        navItem.addEventListener('click', (event)=>{
+            if(event.target.class === nav1){
+                console.log('nav1Clicked!')
+            }
+        })
+    }
+
+
+
+
+
+
+
+// const navItems = document.querySelectorAll(".navItem");
+// const modals = document.querySelectorAll(".modal")
+
+// navItems.forEach(navItems,modals){
+//     navItem=>{
+//         navItem.addEventListener('click', (event)=>{
+//            if(target.event.class === nav1){
+
+//            }else if(target.event.class === nav2){
+
+//            }else if(target.event.class === nav3){
+
+//            }else if(target.event.class === nav4){
+
+//            }else if(target.event.class === nav5){
+
+//            }else if(target.event.class === nav6){
+
+//            }
+//         })
+//     }
+// }
